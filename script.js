69af7eee7b20693205c14ccc51ef1731c9c46eeb
Ignatius22 <igee54077@gmail.com>
2020-07-06 20:18:39 +0100

    add javascript for functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const music = document.querySelector('audio')
+const prevBtn = document.getElementById('prev')
+const playBtn = document.getElementById('play')
+const nextBtn = document.getElementById('next')
+
+
+// play
